9cd60dbaa60628f023dff309c61119c22433c2e6
Zeamari Hamza <zeam.hamza@gmail.com>
2021-09-26 23:06:43 +0200

    suppr

--- src/app/tel-format.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import {TelPipe} from './tel-format.pipe';
-
-describe('TelPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TelPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
